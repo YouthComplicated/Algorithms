42a3af56b03de8cef188319b087e83fd4a4321d5
maolintomscott <2041699001@qq.com>
2018-11-27 23:53:00 +0800

    *)linkList

---
+++ src/base/LinkedList.java
@@ -0,0 +1,149 @@
+package base;
+
+public class LinkedList<E> {
+
+    private class Node{
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next){
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e){
+            this(e, null);
+        }
+
+        public Node(){
+            this(null, null);
+        }
+
+        @Override
+        public String toString(){
+            return e.toString();
+        }
+    }
+
+    //虚拟头结点
+    private Node dummyHead;
+    private  int size;
+
+
+    public  LinkedList(){
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void addFrist(E e){
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+//        head = new Node(e, head);
+        add(0, e);
+        size ++;
+    }
+
+    public void add(int index, E e){
+        if(index < 0 || index > size){
+            throw new IllegalArgumentException("get fail Illegal index！");
+        }
+        Node pre = dummyHead;
+        while ( --index >= 0 ){
+            pre = pre.next;
+        }
+//            Node node = new Node(e);
+//            node.next = pre.next;
+//            pre.next = node;
+        pre.next = new Node(e, pre.next);
+        size ++;
+    }
+
+    public void addLast(E e){
+        add(size, e);
+    }
+
+    public E get(int index){
+        if(index < 0 || index >= size){
+            throw new IllegalArgumentException("get fail Illegal index！");
+        }
+        Node cur  = dummyHead.next;
+        for(int i = 0 ; i < index; i ++ ){
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public E getLast(){
+        return get(size - 1);
+    }
+
+    public E getFrist(){
+        return get(0);
+    }
+
+    public void set(int index, E e){
+        if(index < 0 || index >= size){
+            throw new IllegalArgumentException("get fail Illegal index！");
+        }
+        Node cur  = dummyHead.next;
+        for(int i = 0 ; i < index; i ++ ){
+            cur = cur.next;
+        }
+        cur.e  = e;
+    }
+
+    public boolean constains(E e){
+        Node cur = dummyHead.next;
+        while ( cur != null){
+            if(cur.equals(e)){
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    public E remove(int index){
+        if(index < 0 || index >= size){
+            throw new IllegalArgumentException("get fail Illegal index！");
+        }
+//        E pre = get(index-1);
+        Node prev = dummyHead;
+        for(int i = 0 ; i < index; i++){
+            prev = prev.next;
+        }
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size --;
+        return retNode.e;
+    }
+    public E removeFrist(){
+        return remove(0);
+    }
+
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null){
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+}
