e6a79e5dbe37a45c512786e3d74206bf90423828
maolintomscott <2041699001@qq.com>
2019-01-23 22:47:10 +0800

    *)线程学习2

---
+++ src/Thread/ProAndConsumer/MallQueue.java
@@ -0,0 +1,55 @@
+package Thread.ProAndConsumer;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MallQueue {
+
+    private int count;
+
+    private final int MAX_COUNT = 20;
+
+    private int signal;
+
+    private Lock lock = new ReentrantLock();
+    Condition proCon = lock.newCondition();
+    Condition conCon = lock.newCondition();
+
+    //生产
+    public  void produce(){
+        lock.lock();
+        while(count >= MAX_COUNT) {
+            try {
+                System.out.println(Thread.currentThread().getName() + " 库存数量达到上限，生产者停止生产。");
+                proCon.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        count ++;
+        System.out.println(Thread.currentThread().getName() + " 生产者生产，当前库存为：" + count);
+        conCon.signal();
+//        notifyAll();
+        lock.unlock();
+    }
+
+    //消费
+    public  void consume(){
+        lock.lock();
+        while(count <= 0) {
+            try {
+                System.out.println(Thread.currentThread().getName() + " 库存数量为零，消费者等待。");
+//                wait();
+                conCon.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        count --;
+        System.out.println(Thread.currentThread().getName() + " 消费者消费，当前库存为：" + count);
+//        notifyAll();
+        proCon.signal();
+        lock.unlock();
+    }
+}
