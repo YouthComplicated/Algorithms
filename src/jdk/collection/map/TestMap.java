cf56d01f3226114f4638615f081a7a9a018ec6f2
tomscott <2041699001@qq.com>
2019-03-05 23:13:25 +0800

    testmap

---
+++ src/jdk/collection/map/TestMap.java
@@ -0,0 +1,14 @@
+package jdk.collection.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMap {
+
+    @Test
+    public void test01(){
+        Map<String, Object> map = new HashMap<>();
+    }
+}
