aa9d82e75923e2881b8f23cc643e8fc58a416850
tomscott <1697583989@qq.com>
2019-03-12 19:03:19 +0800

    two problem

---
+++ src/leetcode/SameTree_100.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+/**
+ * @author NJ
+ * @date 2019/3/12 11:43
+ */
+public class SameTree_100 {
+}
---
+++ src/leetcode/SymmetricTree_101.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+/**
+ * @author NJ
+ * @date 2019/3/12 12:52
+ */
+public class SymmetricTree_101 {
+}
