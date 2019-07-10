6b1ed1f12cf6c43249bc6bd8d7b64f62e49aa79e
sunzuo-lanmo <tomsunzuo@gmail.com>
2019-07-10 20:02:49 +0800

    *) 1、阻塞队列测试 2、lettcode +1

---
+++ src/jdk/instance/TestInstanceOf.java
@@ -0,0 +1,38 @@
+package jdk.instance;
+
+import java.util.HashMap;
+
+public class TestInstanceOf {
+
+
+    public static class ParamMap<V> extends HashMap<String, V> {
+
+        private static final long serialVersionUID = -2212268410512043556L;
+
+        @Override
+        public V get(Object key) {
+            if (!super.containsKey(key)) {
+                throw new RuntimeException("Parameter '" + key + "' not found. Available parameters are " + keySet());
+            }
+            return super.get(key);
+        }
+
+    }
+
+
+    public static class StrictMap<V> extends HashMap<String, V> {
+
+        private static final long serialVersionUID = -5741767162221585340L;
+
+        @Override
+        public V get(Object key) {
+            if (!super.containsKey(key)) {
+                throw new RuntimeException("Parameter '" + key + "' not found. Available parameters are " + this.keySet());
+            }
+            return super.get(key);
+        }
+
+    }
+
+
+}
--- src/leetcode/WordPattern_290.java
+++ src/leetcode/WordPattern_290.java
@@ -1,5 +1,10 @@
 package leetcode;
 
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class WordPattern_290 {
 
     /**
@@ -28,16 +33,80 @@ public class WordPattern_290 {
 
     public static void main(String[] args) {
 
+        System.out.println(WordPattern_290.wordPattern("abba","dog cat cat dog"));
+        System.out.println(WordPattern_290.wordPattern("abba","dog cat cat fish"));
+        System.out.println(WordPattern_290.wordPattern("aaaa","dog cat cat dog"));
+        System.out.println(WordPattern_290.wordPattern("abcba","dog t t t dog"));
+
+
     }
 
 
+    public boolean wordPattern1(String pattern, String str) {
+        String[] format = new String[26];
+        boolean[] used = new boolean[26];
+        StringTokenizer st = new StringTokenizer(str);
+        int cnt = 0;
+        while(cnt < pattern.length()) {
+            if(!st.hasMoreTokens()) {
+                return false;
+            }
+            String part = st.nextToken();
+            int pat = pattern.charAt(cnt) - 'a';
+            if(!used[pat]) {
+                for(int i = 0; i < 26; i++) {
+                    if(used[i] && format[i].equals(part)) {
+                        return false;
+                    }
+                }
+                format[pat] = part;
+                used[pat] = true;
+            } else {
+                if(!format[pat].equals(part)) {
+                    return false;
+                }
+            }
+            cnt++;
+        }
+        if(st.hasMoreTokens()) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * my test
+     * @param pattern
+     * @param str
+     * @return
+     */
     public static boolean wordPattern(String pattern, String str) {
         String[] strings = str.split(" ");
-
-
+        int patternLen = pattern.length(), strLen = strings.length;
+        if(patternLen != strLen){
+            return false;
+        }
+        Map<Character, String> map = new HashMap<>();
+        String value;
+        for(int i = 0; i < patternLen; i++){
+            value = map.get(pattern.charAt(i));
+            if(value == null){
+                //判断strings[i]是否为map中value
+                if(map.containsValue(strings[i])){
+                    return false;
+                }else{
+                    map.put(pattern.charAt(i),strings[i]);
+                }
+            }else{
+                if(!value.equals(strings[i])){
+                    return false;
+                }
+            }
+        }
         return true;
-
     }
+
+
 
 
 
