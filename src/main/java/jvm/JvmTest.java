3833cb48815d0662773d86d579af9d0399037914
sunzuo-lanmo <tomsunzuo@gmail.com>
2020-02-09 20:20:08 +0800

    +) something11

---
+++ src/main/java/jvm/JvmTest.java
@@ -0,0 +1,18 @@
+package jvm;
+
+/**
+ * @author: nj
+ * @date: 2020-02-08 10:32
+ * @version: 0.0.1
+ */
+public class JvmTest {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("******main******");
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+    }
+}
